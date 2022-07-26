63eb3d0b94a4db5161ba4f02c627e8b7e88828f3
CHRISTINA SMILLIN <smillin0528@gmail.com>
2022-07-26 17:12:58 +0530

    home page basic design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LiveComponent } from './live/live.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiveComponent
   ],
   imports: [
     BrowserModule,
